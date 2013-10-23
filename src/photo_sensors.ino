edab40c1d0353e9607f0c7624528643376bb4f3e
Omwah <omwah@oyeh.org>
2013-10-22 21:34:46 -0700

    Initial version

---
+++ src/photo_sensors.ino
@@ -0,0 +1,119 @@
+// Control the toy car with photo sensors that move when the average light level changes
+
+// Keep track of a bunch of light levels to keep a running average
+const int NUM_LIGHT_VALS = 10;
+const int NUM_SENSORS = 2;
+
+// Analog pins which are connected to the photo resistors
+int sensors[NUM_SENSORS] = {0, 4}; // A0, A4
+
+int light_levels[NUM_SENSORS][NUM_LIGHT_VALS] = {0};
+bool first_time = false;
+
+const int TRIGGER_THRESH = 10;
+
+// Set up functions to call when sensors are triggered
+// Functions defined in motor_shield.ino
+void left();
+void right();
+void stop();
+
+// Function pointer array of functions to call
+void (*trigger_funcs[NUM_SENSORS]) () = {left, right};
+
+// Set all levels
+void init_light_levels(int curr_levels[])
+{
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        for(int lev = 0; lev < NUM_LIGHT_VALS; lev++) {
+          light_levels[sens][lev] = curr_levels[sens];
+        }
+    }
+}
+
+// Record most recent light levels and return averages
+void average_light_levels(int curr_levels[], int level_avgs[])
+{
+    // Shift all values down one index
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        for(int lev = 0; lev < NUM_LIGHT_VALS; lev++) {  
+          light_levels[sens][lev-1] = light_levels[sens][lev];
+        }
+    }
+
+    // Record latest values
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        light_levels[sens][NUM_LIGHT_VALS-1] = curr_levels[sens];
+    }
+
+    // Create sum for averages
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        level_avgs[sens] = 0;
+        for(int lev = 0; lev < NUM_LIGHT_VALS; lev++) {  
+          level_avgs[sens] += light_levels[sens][lev];
+        }
+        level_avgs[sens] /= NUM_LIGHT_VALS;
+    }
+
+}
+
+void print_debug(int curr_levels[], int level_avgs[]) {
+    Serial.print("avg vals: ");
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        Serial.print(level_avgs[sens]);
+        Serial.print(" ");
+    }
+    Serial.print(", inst vals: ");
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        Serial.print(curr_levels[sens]);
+        Serial.print(" ");
+    }
+    Serial.println();
+}
+
+// Delay a bit when starting up to avoid triggers before
+// sensors have stabilized
+int init_delay = 10;
+
+void loop()
+{
+    // Record sensor values in array
+    int curr_levels[NUM_SENSORS];
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        curr_levels[sens] = analogRead(sensors[sens]);
+    }
+
+    // Initialize the array the first time through the loop
+    // so our average will === current values
+    if(first_time) {
+      first_time = false;
+      init_light_levels(curr_levels);
+    }
+
+    // Average last N samples
+    int avg_levels[NUM_SENSORS];
+    average_light_levels(curr_levels, avg_levels);
+
+    print_debug(curr_levels, avg_levels);
+
+    // Look for trigger conditions
+    int triggered = false;
+    for(int sens = 0; sens < NUM_SENSORS; sens++) {
+        if ((curr_levels[sens] - avg_levels[sens]) > TRIGGER_THRESH && init_delay == 0) {
+            Serial.print("trigger #");
+            Serial.println(sens);
+            trigger_funcs[sens]();
+            triggered = true;
+        } else {
+            stop();
+        }
+
+    }
+
+    if (init_delay > 0) {
+        init_delay--;
+    }
+
+    delay(20);
+
+}
